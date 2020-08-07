c7d40ee85a67eaeee9886b8e2a4dd10b4f2c8a6c
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-08-07 14:49:52 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,16 @@
+# VSIX Item Templates
+
+[![Build status](https://ci.appveyor.com/api/projects/status/4pha1svkn0aqg3u4?svg=true)](https://ci.appveyor.com/project/madskristensen/tweakster)
+
+Awesome item templates for VSIX development
+
+Download this extension from the [Marketplace](https://marketplace.visualstudio.com/items?itemName=MadsKristensen.Tweaks)
+or get the [CI build](https://www.vsixgallery.com/extension/88049e1e-62f2-4ea2-851f-9ddb2de37f41).
+
+----------------------------------------------
+
+Coming soon...
+
+
+## License
+[Apache 2.0](LICENSE)
---
+++ VsixItemTemplates/ItemTemplates/TextviewCreationListener/TextviewCreationListener.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace $rootnamespace$
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class $safeitemname$ : IWpfTextViewCreationListener
+    {
+        /// <summary>
+        /// Called when a text view having matching roles is created over a text data model having a matching content type.
+        /// </summary>
+        /// <param name="textView">The <see cref="IWpfTextView"/> that has been created.</param>
+        public void TextViewCreated(IWpfTextView textView)
+        {
+
+        }
+    }
+}
