750a00fca40ccad5e0010592ba2843d2eee7b90b
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-08-28 08:09:50 -0700

    Added EditorCommandHandler item template

---
+++ VsixItemTemplates/ItemTemplates/EditorCommandHandler/EditorCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Tweakster.Tweaks.CodeCleanupOnFormat
+{
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof($safeitemname$))]
+    [ContentType("code")]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    public class $safeitemname$ : ICommandHandler<EditorCommandArgs> // Replace EditorCommandArgs with more specific CommandArgs class
+    {
+        public string DisplayName => nameof($safeitemname$);
+
+        public bool ExecuteCommand(EditorCommandArgs args, CommandExecutionContext executionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CommandState GetCommandState(EditorCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+    }
+}
