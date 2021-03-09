4b3d332a821477ee27e62b3e1ba9915e69392938
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-03-09 13:21:17 -0800

    Removed pkgdef file

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace VsixItemTemplates
     internal sealed partial class Vsix
     {
         public const string Id = "88049e1e-62f2-4ea2-851f-9ddb2de37f41";
-        public const string Name = "Extensibility Templates";
+        public const string Name = "Extensibility Template Pack";
         public const string Description = @"A template pack for Visual Studio extension authors full of useful project- and item templates.";
         public const string Language = "en-US";
         public const string Version = "1.1";
