20fbc2d3ef9f960c4cab13e07628eadc11e10caa
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-07-09 15:26:21 -0700

    Added My Company project template

---
+++ src/2022/ProjectTemplates/MyCompany/Commands/UrlCommandFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+namespace $safeprojectname$
+{
+    internal sealed class UrlCommandFactory
+    {
+        private readonly IMenuCommandService _commandService;
+
+        private UrlCommandFactory(IMenuCommandService commandService)
+        {
+            _commandService = commandService;
+        }
+
+        public static async Task<UrlCommandFactory> CreateAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            var commandService = (IMenuCommandService)await package.GetServiceAsync(typeof(IMenuCommandService));
+            return new UrlCommandFactory(commandService);
+        }
+
+        internal void Register(int commandId, string url)
+        {
+            var cmdId = new CommandID(PackageGuids.$safeprojectname$, commandId);
+            var cmd = new MenuCommand((s, e) => Execute(url), cmdId);
+            _commandService.AddCommand(cmd);
+        }
+
+        private static void Execute(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
---
+++ src/2022/ProjectTemplates/MyCompany/MyCompanyPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace $safeprojectname$
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuids.$safeprojectname$String)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideBindingPath]
+    public sealed class MyCompanyPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            UrlCommandFactory factory = await UrlCommandFactory.CreateAsync(this);
+
+            factory.Register(PackageIds.Wiki, "https://example.com?page=wiki");
+            factory.Register(PackageIds.BuildServer, "https://example.com?page=buildserver");
+            factory.Register(PackageIds.HrPortal, "https://example.com?page=hrportal");
+        }
+    }
+}
